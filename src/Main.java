18bba596c98d813fde0cccb37e4b54180fc28e76
Simone <simonecabral.br@gmail.com>
2024-11-18 21:25:57 -0300

    Correção de permissões do git

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Teste para validar solução de desafio
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Por favor informar o saldo atual");
